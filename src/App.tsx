a9d0936ec3aef27ebd816c9c2a1f15c47d76a8f3
Oleksandr Zadvornyi <zadvornyi.alex16@gmail.com>
2025-02-16 23:34:32 +0200

    feat: add message on top of the landing page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,9 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="bg-gray-100 p-4 rounded-lg shadow-md text-center flex flex-col items-center gap-2">
-      <h1 className="text-2xl font-bold text-gray-800">Hello, Tailwind!</h1>
-      <p className="text-gray-700">This is a React app with Tailwind CSS.</p>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Click me
-      </button>
-    </div>
+    <p className="text-center my-3.5 text-secondary font-normal text-[14px] leading-[21px] font-primary">
+      Get 10% Off Your First Order with Code WELCOME10
+    </p>
   );
 }
 
