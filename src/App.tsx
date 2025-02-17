c80ed8801126532ead5c0f97c2ab207baa1bbd83
Oleksandr Zadvornyi <zadvornyi.alex16@gmail.com>
2025-02-17 16:00:14 +0200

    fix: replace discount message with React component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import DiscountMessage from "./components/DiscountMessage";
 
 function App() {
   return (
-    <p className="text-center my-3.5 text-secondary font-normal text-[14px] leading-[21px] font-primary">
-      Get 10% Off Your First Order with Code WELCOME10
-    </p>
+    <header>
+      <DiscountMessage />
+    </header>
   );
 }
 
---
+++ src/components/DiscountMessage.tsx
@@ -0,0 +1,9 @@
+function DiscountMessage() {
+  return (
+    <p className="text-center my-3.5 text-secondary font-normal text-[14px] leading-[21px] font-primary">
+      Get 10% Off Your First Order with Code WELCOME10
+    </p>
+  );
+}
+
+export default DiscountMessage;
