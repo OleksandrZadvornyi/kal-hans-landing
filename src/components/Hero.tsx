b3da532dc108f8ebfa58cf74430673e0d715e107
Oleksandr Zadvornyi <zadvornyi.alex16@gmail.com>
2025-02-20 23:22:24 +0200

    fix: show `.avif` or `.webp` hero img if possible

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,17 @@
+import avif from '../assets/images/hero.avif';
+import webp from '../assets/images/hero.webp';
+import jpg from '../assets/images/hero.jpg';
+
 function Hero() {
+  const backgroundImage = [`url(${avif})`, `url(${webp})`, `url(${jpg})`].join(
+    ', '
+  );
+
   return (
-    <div className="relative min-h-[600px] bg-[url(/src/assets/images/hero.jpg)] bg-cover bg-center">
+    <div
+      className="relative min-h-[600px] bg-cover bg-center"
+      style={{ backgroundImage }}
+    >
       <p className="bg-primary absolute bottom-7 left-7 inline-block px-11 py-2.5 text-[14px] leading-5">
         Shop NEW Summer Collection!
       </p>
