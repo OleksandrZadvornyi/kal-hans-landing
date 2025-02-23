aeb3d898c2e6ac637594216dfe521a485db7ae2e
Oleksandr Zadvornyi <zadvornyi.alex16@gmail.com>
2025-02-23 15:07:54 +0200

    fix: add vertical padding to intro section

--- src/components/AboutIntro.tsx
+++ src/components/AboutIntro.tsx
@@ -1,6 +1,6 @@
 function AboutIntro() {
   return (
-    <section className="bg-primary flex min-h-[560px] flex-col items-center justify-center px-9 text-center">
+    <section className="bg-primary flex min-h-[560px] flex-col items-center justify-center px-9 py-20 text-center">
       <h2 className="text-[42px]">
         We Make Feeling Comfortable in Your Own Skin Natural
       </h2>
